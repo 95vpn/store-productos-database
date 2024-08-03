3d113aa724364aaa36e5566ebbef63e1bcb21fd1
Johnny Herrera <jmjaramillo95@gmail.com>
2024-08-02 23:58:01 -0500

    model-product

---
+++ src/models/Product.js
@@ -0,0 +1,24 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection');
+
+const Product = sequelize.define('product', {
+    nombre_product: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    //categoryId
+});
+
+module.exports = Product;
